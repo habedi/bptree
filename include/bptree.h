9121e9f0b002777b121b446dff11d5a4ccbf2f63
Hassan Abedi <hassan.abedi.t@gmail.com>
2026-01-10 19:02:33 +0100

    WIP

--- include/bptree.h
+++ include/bptree.h
@@ -43,7 +43,7 @@
  *   - This implementation is NOT thread-safe. Caller must provide external
  *     synchronization (e.g., mutexes) for concurrent access.
  *
- * @version 0.4.1-beta
+ * @version 0.4.3
  * @author
  *   "Hassan Abedi <hassan.abedi.t+bptree@gmail.com>"
  * @copyright MIT License

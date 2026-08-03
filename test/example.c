6b40540af3246c49041f19791f54da7a51c1f7b4
Hassan Abedi <hassan.abedi.t@gmail.com>
2026-08-03 19:28:16 +0200

    The base commit

--- test/example.c
+++ test/example.c
@@ -7,7 +7,7 @@
  * removing records, and updating records using the bptree library.
  * It uses numeric keys and stores pointers to a custom `record_t` struct as values.
  *
- * @version 0.5.0
+ * @version 0.5.1
  */
 
 #define BPTREE_IMPLEMENTATION             // Include implementation of B+ tree
